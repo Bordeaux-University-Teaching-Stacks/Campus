afc2644a995f6527280c43f6abe3473200f95895
Meidi Airouche <meidi.airouche@gmail.com>
2014-07-14 00:24:37 +0200

    MAJ des tests

---
+++ campus/src/test/java/com/intuitiv/campus/test/controller/TrombinoscopeTest.java
@@ -0,0 +1,30 @@
+package com.intuitiv.campus.test.controller;
+
+import static org.springframework.test.web.ModelAndViewAssert.assertViewName;
+
+import org.junit.Test;
+
+public class TrombinoscopeTest extends AbstractControllerTest{
+
+	@Test
+	public void testTrombinoscopeWithSession() throws Exception {
+		request.setRequestURI("/user/login");
+		request.setMethod("POST");
+		request.addParameter("email", "select@campus.fr");
+		request.addParameter("password", "Test13");
+		getResponsePage();
+		request.setRequestURI("/trombinoscope/");
+		request.setMethod("GET");
+
+		assertViewName(getResponsePage(), "Trombinoscope");
+	}
+
+	@Test
+	public void testTrombinoscopeWithNoSession() throws Exception {
+		request.setRequestURI("/trombinoscope/");
+		request.setMethod("GET");
+
+		assertViewName(getResponsePage(), "Home");
+	}
+
+}
